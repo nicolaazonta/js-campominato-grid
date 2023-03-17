2c1d7e8153dd1abbfb251416e9080a834468ef5e
nicolaazonta <nicolazonta114@gmail.com>
2023-03-17 22:22:47 +0100

    toggle on click

--- assets/js/mani.js
+++ assets/js/mani.js
@@ -14,15 +14,63 @@ con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 
 
 const createButtonEl = document.getElementById('create_button')
 
-createButtonEl.addEventListener('click' , function(){
+
+
+/* const difficultAnchor = document.getElementById('difficult_choice');
+
+const difficultChoice = difficultAnchor.value;
+
+console.log(difficultChoice); */
+
+
+
+
+/* var e = document.getElementById("ddlViewBy");
+var value = e.value;
+var text = e.options[e.selectedIndex].text; */
+
+//if()
+
+
+createButtonEl.addEventListener('click' , function(){   
+
     
-    for (let i = 0; i < 100; i++) {
+    const difficultChoice = Number(document.getElementById('difficult_choice').value);
+
+    let difficultGradient; 
+    let numberOfSquares;
+
+    if(difficultChoice === 1){
+        difficultGradient = 'square_easy';
+        numberOfSquares = 100;
+    } else if(difficultChoice === 2){
+        difficultGradient = 'square_medium';
+        numberOfSquares = 81;
+    } else if(difficultChoice === 3){
+        difficultGradient = 'square_difficult';
+        numberOfSquares = 49;
+    } 
+
+    for (let i = 0; i < numberOfSquares; i++) {
         const containerEl = document.querySelector('.container');
-        const creatingSquare = `<div class="square d-flex justify-content-center align-items-center"><h5>${i + 1}</h5>
+        const creatingSquare = `<div class="square ${difficultGradient} d-flex justify-content-center align-items-center"><h5>${i + 1}</h5>
         </div>`
         containerEl.insertAdjacentHTML('beforeend' , creatingSquare);    
     }
-    
+
+    const allSquares = document.querySelectorAll('.square');
+
+    for (let i = 0; i < allSquares.length ; i++) {
+
+        let thisCell = allSquares[i];
+
+        thisCell.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            //console.log(allSquares[i]);
+        })
+        
+    }
+
 })
 
 
