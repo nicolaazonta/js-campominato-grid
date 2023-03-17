0627babfac0b0b572e09d54b50ce5913a0614e12
nicolaazonta <nicolazonta114@gmail.com>
2023-03-17 15:55:46 +0100

    create grid elements with html then with js

--- assets/js/mani.js
+++ assets/js/mani.js
@@ -1,1 +1,24 @@
-alert('OCIO')
+/* Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
+
+//creare una griglia di 100 caselle
+
+for (let i = 0; i < 100; i++) {
+    const containerEl = document.querySelector('.container');
+    const creatingSquare = `<div class="square d-flex justify-content-center align-items-center"><span>${i + 1}</span>
+    </div>`
+    containerEl.insertAdjacentHTML('beforeend' , creatingSquare);
+    
+
+    
+}
+
+
